c7d4ec5702e58bee12771246330fb9be75aef56e
Alexander Kiel <alexanderkiel@gmx.net>
2015-07-17 16:26:34 +0200

    Fixed Ref Import

--- src/lens/import.clj
+++ src/lens/import.clj
@@ -52,12 +52,16 @@
       (update :count dec)))
 
 (defn- create-item-group-ref-job [defs ref]
+  (log/trace "create-item-group-ref-job:" ref)
   (let [form-def (get-in defs [:form-defs (:form-def-id ref)])]
+    (assert form-def)
     (->> (async/chan 1 (assoc-xf :type :item-group-ref))
          (async/pipe (api/create-item-group-ref! form-def ref)))))
 
 (defn- create-item-ref-job [defs ref]
+  (log/trace "create-item-ref-job:" ref)
   (let [item-group-def (get-in defs [:item-group-defs (:item-group-def-id ref)])]
+    (assert item-group-def)
     (->> (async/chan 1 (assoc-xf :type :item-ref))
          (async/pipe (api/create-item-ref! item-group-def ref)))))
 
@@ -108,24 +112,25 @@
   (update state :jobs #(remove-job % :item-group port)))
 
 (defn handle-item-group-def-job [state port val]
+  (log/trace "handle-item-group-def-job:" (:id (:data val)))
   (let [create-ref (map #(create-item-group-ref-job (get-in state [:defs (:study-id val)]) %))
-        ref-vec [(:study-id val) :item-group-refs (:id (:data val))]
-        ref-jobs (eduction create-ref (get-in state [:refs ref-vec]))]
+        ref-vec [:refs (:study-id val) :item-group-refs (:id (:data val))]
+        ref-jobs (eduction create-ref (get-in state ref-vec))]
     (-> (update state :jobs #(-> (remove-job % :item-group port)
                                  (add-jobs :item-group ref-jobs)))
         (update :defs #(collect-item-group-def % val))
-        (update :refs #(dissoc-in % ref-vec)))))
+        (dissoc-in ref-vec))))
 
 (defn handle-item-ref-job [state port _]
   (update state :jobs #(remove-job % :item port)))
 
 (defn handle-item-def-job [state port val]
-  (let [create-ref (map #(create-item-ref-job (get-in state [:defs (:study-id val)]) %))
-        ref-vec [(:study-id val) :item-refs (:id (:data val))]
-        ref-jobs (eduction create-ref (get-in state [:refs ref-vec]))]
+  (let [create-ref (map (fn [ref] (create-item-ref-job (get-in state [:defs (:study-id val)]) ref)))
+        ref-vec [:refs (:study-id val) :item-refs (:id (:data val))]
+        ref-jobs (eduction create-ref (get-in state ref-vec))]
     (-> (update state :jobs #(-> (remove-job % :item port)
                                  (add-jobs :item ref-jobs)))
-        (update :refs #(dissoc-in % ref-vec)))))
+        (dissoc-in ref-vec))))
 
 (defn- job-handler [type]
   (case type
@@ -137,7 +142,12 @@
     :item-ref handle-item-ref-job
     :item-def handle-item-def-job))
 
-(defn import! [service-document n parse-ch]
+(defn import!
+  "Returns a channel which closes after the import finished.
+
+  Puts an ExceptionInfo onto the channel if there is any problem. Fails fast on
+  the first problem."
+  [service-document n parse-ch]
   (let [constrain-jobs
         (fn [state]
           (if (< (:count (:jobs state)) n) [parse-ch] []))]
@@ -147,14 +157,17 @@
                      :defs {}
                      :refs {}}]
       (let [[val port] (async/alts! (into ports (select-jobs (:jobs state))))]
-        (if-let [type (:type val)]
-          (if (= parse-ch port)
-            (if (= :study type)
-              (recur [] (handle-study state val))
-              (recur (constrain-jobs state) ((handler type) state val)))
-            (recur (constrain-jobs state) ((job-handler type) state port val)))
+        (if (instance? Throwable val)
+          val
+          (if-let [type (:type val)]
+            (if (= parse-ch port)
+              (if (= :study type)
+                (recur [] (handle-study state val))
+                (recur (constrain-jobs state) ((handler type) state val)))
+              (recur (constrain-jobs state) ((job-handler type) state port val)))
 
-          (if (= 0 (:count (:jobs state)))
-            (log/info "Finished!")
-            (recur [] state)))))))
-
+            (if (= 0 (:count (:jobs state)))
+              (if (empty? (:refs state))
+                (log/info "Finished!")
+                (throw (ex-info (str "Unresolved refs: " (:refs state)) {:refs (:refs state)})))
+              (recur [] state))))))))
